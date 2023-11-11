0b42c019acca993198744a9010fb87b1a113e109
BrauuHdzm <brauuhdzm@gmail.com>
2023-11-10 23:57:08 -0600

    Redireccionamientos corregidos para NodeJS

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Directorio público para archivos estáticos (CSS, JS, imágenes, etc.)
-// Asegúrate de que la carpeta 'assets' exista en la raíz de tu proyecto
 app.use(express.static('assets'));
 
 // Función para obtener una conexión a la base de datos
@@ -57,13 +56,69 @@ app.post('/register', async (req, res) => {
   }
 });
 
+
+// Ruta de inicio de sesión
+app.get('/login', async (req, res) => {
+    res.sendFile(__dirname + '/login.html');
+});
+
+// Ruta de pagina principal de articulos
+app.get('/articulos', async (req, res) => {
+    res.sendFile(__dirname + '/articulos.html');
+});
+
+// Ruta para nuevo articulo
+app.get('/nuevo-articulo', async (req, res) => {
+    res.sendFile(__dirname + '/nuevo-articulo.html');
+});
+
+// Ruta para articulo-generado
+app.get('/articulo-generado', async (req, res) => {
+    res.sendFile(__dirname + '/articulo-generado.html');
+});
+
+// Ruta para articulos-encontrados
+app.get('/articulos-encontrados', async (req, res) => {
+    res.sendFile(__dirname + '/articulos-encontrados.html');
+});
+
+// Ruta para como-funciona
+app.get('/como-funciona', async (req, res) => {
+    res.sendFile(__dirname + '/como-funciona.html');
+});
+
+// Ruta para contacto
+app.get('/contacto', async (req, res) => {
+    res.sendFile(__dirname + '/contacto.html');
+});
+
+// Ruta para contrasena-olvidada
+app.get('/contrasena-olvidada', async (req, res) => {
+    res.sendFile(__dirname + '/contrasena-olvidada.html');
+});
+
+// Ruta para dato a modificar
+app.get('/dato-modificar', async (req, res) => {
+    res.sendFile(__dirname + '/dato-modificar.html');
+});
+
+// Ruta para eliminar-cuenta
+app.get('/eliminar-cuenta', async (req, res) => {
+    res.sendFile(__dirname + '/eliminar-cuenta.html');
+});
+
+// Ruta para calificacion
+app.get('/calificacion', async (req, res) => {
+    res.sendFile(__dirname + '/calificacion.html');
+});
+
 // Ruta GET para servir la página de registro
 app.get('/registrarse', (req, res) => {
   // Asegúrate de que el archivo 'registrarse.html' exista en la carpeta 'assets'
   res.sendFile(__dirname + '/registrarse.html');
 });
 
-// Ruta principal (opcional)
+// Ruta principal 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/main.html');
 });
